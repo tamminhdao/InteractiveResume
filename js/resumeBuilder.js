d019407e4a5a10ca4b2636a69759be51e245c915
tamminhdao <tamminhdaoabbas@gmail.com>
2016-11-17 23:52:13 -0600

    final

---
+++ js/resumeBuilder.js
@@ -0,0 +1,265 @@
+
+var bio = {
+    "name" : "Tam Dao",
+    "role": "Front-end Developer",
+    "contacts" : {
+        "email": "tam@gmail.com",
+        "mobile": "920-312-6969",
+        "github": "tamminhdao",
+        "twitter": "@tamminhdao",
+        "location": "Florida, USA"
+    },
+    "welcomeMessage": "Viva le Vida! Praesent vestibulum leo eu quam convallis molestie.",
+    "skills": ["HTML", "CSS", "JavaScript", "Python", "Git"],
+    "biopic" : "images/bioPic.jpg"
+};
+
+
+var education = {
+    "schools": [{
+        "name": "Lawrence University",
+        "location": "Appleton, Wisconsin",
+        "degree": "Bachelor of Sciences",
+        "majors": ["Economics,", "Environmental Studies"],
+        "dates": "2006-2010",
+        "url": "http://www.lawrence.edu"
+    }, {
+        "name": "RMIT University",
+        "location": "Hanoi, Vietnam",
+        "degree": "Bachelor of Arts",
+        "majors": ["English"],
+        "dates": "2003-2006",
+        "url": "http://www.rmit.edu.vn"
+    }, ],
+    "onlineCourses": [{
+        "title": "Intro to HTML & CSS",
+        "school": "Codecademy",
+        "dates": "2014",
+        "url": "http://www.codecademy.com"
+    }, {
+        "title": "Intro to Python",
+        "school": "Treehouse",
+        "dates": "2015",
+        "url": "http://www.teamtreehouse.com"
+    }, {
+        "title": "How to use Git and Github",
+        "school": "Udacity",
+        "dates": "2016",
+        "url": "http://www.udacity.com"
+    }]
+};
+
+
+var work = {
+    "jobs": [{
+        "employer": "The Asia Foundation",
+        "title": "Program Officer",
+        "dates": "2012-2014",
+        "location": "Bejing, China",
+        "description": "In eu semper felis. Vestibulum fermentum risus a turpis pellentesque, eu dignissim justo feugiat. Fusce porttitor, eros ac eleifend volutpat, ipsum erat congue nisl, in finibus erat lorem quis nunc. Donec eu leo mi. Curabitur dui risus, interdum quis orci vitae, vestibulum consequat neque. Duis tincidunt tincidunt diam, eu ultricies justo volutpat et. Vestibulum imperdiet aliquet lacus, sed varius augue dictum vel."
+    }, {
+        "employer": "The Mekong River Commission",
+        "title": "Junior Riparian Officer",
+        "dates": "2011",
+        "location": "Vientiane, Laos",
+        "description": "In eu semper felis. Vestibulum fermentum risus a turpis pellentesque, eu dignissim justo feugiat. Fusce porttitor, eros ac eleifend volutpat, ipsum erat congue nisl, in finibus erat lorem quis nunc. Donec eu leo mi. Curabitur dui risus, interdum quis orci vitae, vestibulum consequat neque. Duis tincidunt tincidunt diam, eu ultricies justo volutpat et. Vestibulum imperdiet aliquet lacus, sed varius augue dictum vel."
+    }],
+    "internships": [{
+        "employer": "Center for Neighborhood",
+        "title": "Communication Intern",
+        "dates": "2010",
+        "location": "Denver, CO",
+        "description": "In eu semper felis. Vestibulum fermentum risus a turpis pellentesque, eu dignissim justo feugiat. Fusce porttitor, eros ac eleifend volutpat, ipsum erat congue nisl, in finibus erat lorem quis nunc. Donec eu leo mi. Curabitur dui risus, interdum quis orci vitae, vestibulum consequat neque. Duis tincidunt tincidunt diam, eu ultricies justo volutpat et. Vestibulum imperdiet aliquet lacus, sed varius augue dictum vel."
+    }, {
+        "employer": "Water Action",
+        "title": "Conservation Intern",
+        "dates": "2010",
+        "location": "San Francisco, CA",
+        "description": "In eu semper felis. Vestibulum fermentum risus a turpis pellentesque, eu dignissim justo feugiat. Fusce porttitor, eros ac eleifend volutpat, ipsum erat congue nisl, in finibus erat lorem quis nunc. Donec eu leo mi. Curabitur dui risus, interdum quis orci vitae, vestibulum consequat neque. Duis tincidunt tincidunt diam, eu ultricies justo volutpat et. Vestibulum imperdiet aliquet lacus, sed varius augue dictum vel."
+    }]
+};
+
+
+var projects = {
+    "projects": [{
+        "title": "Mockup to Website",
+        "dates": "2016",
+        "description": "You will be provided with a design mockup as a PDF-file and must replicate that design in HTML and CSS. You will develop a responsive website that will display images, descriptions and links to each of the portfolio projects you will complete throughout the course of the Front-End Web Developer Nanodegree.",
+        "images": ["https://unsplash.it/g/400/300/?image=885", "https://unsplash.it/400/300/?image=0"]
+    }, {
+        "title": "Interactive Resume",
+        "dates": "2016",
+        "description": "You will develop an interactive resume application that reads your resume content from a JSON file and dynamically displays that content within a provided template. You will use objects, functions, conditionals, and control structures to compose the content that will display on the resume.",
+        "images": ["https://unsplash.it/g/400/300/?image=998", "https://unsplash.it/400/300/?image=1059"]
+    }, {
+        "title": "Classic Arcade Game Clone",
+        "dates": "2016",
+        "description": "You will be provided with visual assets and a game loop engine; using these tools you must add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger.",
+        "images": ["https://unsplash.it/g/400/300/?image=357", "https://unsplash.it/400/300/?image=383"]
+    }, {
+        "title": "Website Optimization",
+        "dates": "2016",
+        "description": "You will optimize a provided website with a number of optimization- and performance-related issues so that it achieves a target PageSpeed score and runs at 60 frames per second.",
+        "images": ["https://unsplash.it/g/400/300/?image=893", "https://unsplash.it/400/300/?image=739"]
+    }, {
+        "title": "Neighborhood Map Project",
+        "dates": "2016",
+        "description": "You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that provide additional information about each of these locations such as StreetView images, Wikipedia articles, Yelp reviews, etc.",
+        "images": ["https://unsplash.it/g/400/300/?image=899", "https://unsplash.it/400/300/?image=576"]
+    }, {
+        "title": "Feed Reader Testing",
+        "dates": "2016",
+        "description": "In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they’ve already included Jasmine and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we’re now left with an application with an incomplete test suite. That’s where you come in.",
+        "images": ["https://unsplash.it/g/400/300/?image=861", "https://unsplash.it/400/300/?image=403"]
+    }]
+};
+
+
+
+/*---------------------------------------------------------------
+                        DISPLAY BIO
+----------------------------------------------------------------*/
+
+bio.display = function () {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole. replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+
+    var formattedEmail = HTMLemail.replace ("%data%", bio.contacts.email);
+    var formattedMobile = HTMLmobile.replace ("%data%", bio.contacts.mobile);
+    var formattedGithub = HTMLgithub.replace ("%data%", bio.contacts.github);
+    var formattedTwitter = HTMLtwitter.replace ("%data%", bio.contacts.twitter);
+    var formattedLocation = HTMLlocation.replace ("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedEmail, formattedMobile, formattedGithub, formattedTwitter, formattedLocation);
+    $("#footerContacts").append(formattedEmail, formattedMobile, formattedGithub, formattedTwitter, formattedLocation);
+
+    var formattedWelcomeMsg = HTMLwelcomeMsg. replace ("%data%", bio.welcomeMessage);
+    var formattedBioPic = HTMLbioPic.replace ("%data%", bio.biopic);
+    $("#header").append(formattedBioPic);
+    $("#header").append(formattedWelcomeMsg);
+
+    if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        for (i=0; i<bio.skills.length; i++) {
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+};
+
+bio.display();
+
+
+/*---------------------------------------------------------------
+                        DISPLAY WORK EXPERIENCE
+----------------------------------------------------------------*/
+
+work.display = function () {
+    for (var key in work) {
+        //check if work[key] is an array or not to avoid iterating over the key "display" i.e. this function itself
+        if (work.hasOwnProperty(key) && Array.isArray (work[key])) {
+            work[key].forEach (function(item) { //iterate through each object in the [jobs] and [internships] array.
+                $("#workExperience").append(HTMLworkStart);
+                var formattedEmployer = HTMLworkEmployer.replace ("%data%", item.employer);
+                var formattedTitle = HTMLworkTitle.replace("%data%", item.title);
+                var formattedWorkDates = HTMLworkDates.replace("%data%", item.dates);
+                var formattedWorkLocation = HTMLworkLocation.replace ("%data%", item.location);
+                var formattedWorkDescription = HTMLworkDescription.replace ("%data%", item.description);
+
+                $(".work-entry:last").append(formattedEmployer + formattedTitle);
+                $(".work-entry:last").append(formattedWorkDates + formattedWorkLocation);
+                $(".work-entry:last").append(formattedWorkDescription);
+            });
+        }
+    }
+};
+
+work.display();
+
+
+/* WITHOUT ENCAPSULATION, WE WOULDNT HAVE TO WORRY ABOUT ITERATING OVER A FUNCTION
+
+function displayWork () {
+    Object.keys(work).forEach(function(key) { //iterate through every key in the {work} object
+        work[key].forEach (function(item) { //iterate through each object in the [jobs] and [internships] array.
+            $("#workExperience").append(HTMLworkStart);
+            var formattedEmployer = HTMLworkEmployer.replace ("%data%", item.employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", item.title);
+            var formattedWorkDates = HTMLworkDates.replace("%data%", item.dates);
+            var formattedWorkLocation = HTMLworkLocation.replace ("%data%", item.location);
+            var formattedWorkDescription = HTMLworkDescription.replace ("%data%", item.description);
+
+            $(".work-entry:last").append(formattedEmployer + formattedTitle);
+            $(".work-entry:last").append(formattedWorkDates + formattedWorkLocation);
+            $(".work-entry:last").append(formattedWorkDescription);
+        });
+    });
+}
+*/
+
+/*---------------------------------------------------------------
+                        DISPLAY PROJECTS
+----------------------------------------------------------------*/
+projects.display = function () {
+    projects.projects.forEach (function(item) { //iterate through each item (object) in the [projects.projects] array
+        $("#projects").append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace ("%data%", item.title);
+        var formattedDescription = HTMLprojectDescription.replace ("%data%", item.description);
+        $(".project-entry:last").append(formattedTitle);
+        $(".project-entry:last").append(formattedDescription);
+        item.images.forEach (function (image) {
+            var formattedImage = HTMLprojectImage.replace("%data%", image);
+            $(".project-entry:last").append(formattedImage);
+        });
+    });
+};
+
+projects.display();
+
+
+
+/*---------------------------------------------------------------
+                        DISPLAY EDUCATION
+----------------------------------------------------------------*/
+
+education.display = function () {
+    education.schools.forEach (function(item) { //iterate through each item (object) in the [education.schools] array
+        $("#education").append(HTMLschoolStart);
+        var formattedSchoolName = HTMLschoolName.replace("#", item.url).replace("%data%", item.name);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", item.degree);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", item.dates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", item.location);
+        var collegeMajors = '';
+        item.majors.forEach (function(major) {
+            collegeMajors = collegeMajors + ' ' + major;
+        });
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", collegeMajors);
+
+        $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+        $(".education-entry:last").append(formattedSchoolDates, formattedSchoolLocation, formattedSchoolMajor);
+
+    });
+
+        $(".education-entry:last").append(HTMLonlineClasses);
+        education.onlineCourses.forEach(function(item) {
+            var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", item.title).replace("#", item.url);
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", item.school);
+            var formattedOnlineDates = HTMLonlineDates.replace("%data%", item.dates);
+            var formattedOnlineURL = HTMLonlineURL.replace("%data%", item.url).replace("#", item.url);
+            $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+            $(".education-entry:last").append(formattedOnlineDates, formattedOnlineURL);
+        });
+};
+
+education.display();
+
+
+/*---------------------------------------------------------------
+                        DISPLAY MAP
+----------------------------------------------------------------*/
+
+
+$("#mapDiv").append(googleMap);
+
+
